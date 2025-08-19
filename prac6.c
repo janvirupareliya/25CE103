9bbc5a7435f018d90f7d5363e2820a069bab9ce1
janvirupareliya <janvirupareliya1312@gmail.com>
2025-08-19 11:19:42 +0530

    Add files via upload

---
+++ prac6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    float F,C;
+    printf("enter temp. in C: ");
+    scanf("%f",&C);
+    F=(C* 9/5)+32;
+    printf("temp in F is %f",F);
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
