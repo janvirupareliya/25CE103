c95ae657837b5aa584060bc2a2b42a7edf2c58de
janvirupareliya <janvirupareliya1312@gmail.com>
2025-08-19 11:19:59 +0530

    Add files via upload

---
+++ prac7.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+
+    printf("enter a age: ");
+    scanf("%d", &age);
+
+    if(age<12)
+    {
+      printf("you are eligible for free ticket");
+
+
+    }
+    else
+    {
+       printf("you pay ticket price is 500rs");
+
+    }
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
