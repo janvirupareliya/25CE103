520a94baff8606dcc20cb430fd28be90aa7f49e0
janvirupareliya <janvirupareliya1312@gmail.com>
2025-08-28 15:13:52 +0530

    Add files via upload

---
+++ prac13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int water=10;
+ printf("Smart Water System Company\n\n");
+
+   for(water=10;water<=100;water=water+10)
+   {
+     printf("current water level:%d liters\n",water);
+   }
+   printf("\nTank is full");
+
+return 0;
+}
