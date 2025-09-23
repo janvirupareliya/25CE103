9e0c034d71832e414fd15a7e81a9e000493186d6
janvirupareliya <janvirupareliya1312@gmail.com>
2025-09-23 11:02:16 +0530

    Add files via upload

---
+++ prac19,2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n=5,j,i;
+    for(i=1; i<=n; i++)
+    {
+        for(j = 1; j <= n*2 - 1; j++)
+        {
+            if(j == i || j == (2*n - i))
+                printf("%d",j);
+            else if(i==1)
+                printf("%d",j);
+            else
+                printf(" ");
+
+        }
+        printf("\n");
+
+    }
+    return 0;
+
+
+}
