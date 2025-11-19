fd1de56a29c56434ea2c42380b9e53a5f9c12084
janvirupareliya <janvirupareliya1312@gmail.com>
2025-11-19 23:44:53 +0530

    Add files via upload

---
+++ practical_02.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,p,a;
+    printf("Enter the Length: \n");
+    scanf("%d",&l);
+    printf("Enter the Breadth: \n");
+    scanf("%d",&b);
+    a=l*b;
+    p=2*(l+b);
+    printf("area =%d \n",a);
+    printf("perimeter =%d \n\n",p);
+    printf("submitted by JANVI RUPARELIYA\nID NO: 25CE103");
+    return 0;
+}
